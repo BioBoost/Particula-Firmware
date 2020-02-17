c69e7e75347f7c6e29da96a1e4fdaa3d15d68421
Bram Soenens <bramsoenens@hotmail.be>
2020-02-17 12:47:58 +0100

    Start basic driver 2

--- TPH/tphInterface.cpp
+++ TPH/tphInterface.cpp
@@ -4,7 +4,7 @@
 
 //For IC2 intilization
 PinName sda = D0;
-PinName sck = D1;
+PinName scl = D1;
 //SDA=DO and SCK=D1 for NUCLEO_L432KC
 //SDA=D14 and SCK=D15 for NUCLEO_F042K6
 
@@ -13,19 +13,19 @@ Serial pc(USBTX, USBRX);                        //tx, rx
 
 void tphInterface(char addr){                   //Input of a 7BIT I2C address
     addr << 1;                                  //Shifting the address 1 bit to get an 8BIT addres that embed is using
-    BME280 TphIc2 = BME280(sda, sck, addr);     //Initializing sensor with library
+    BME280 sensorTPH = BME280(sda, scl, addr);  //Initializing sensor with library
 }
 
-double temperature(){
-    //WIP
+double temperature(){                           //returns Termperature as a double in °C    
+    return (double)sensorTPH.getTeperature();
 }
 
-double presure(){
-    //WIP
+double presure(){                               //returns Presure as a double in hPa
+    return (double)sensorTPH.getPresure();
 }
 
-double humidity(){
-    //WIP
+double humidity(){                              //returns Humidity as a doubel in %
+    return (double)sensorTPH.getHumidity();
 }
 
 void sleep(){
