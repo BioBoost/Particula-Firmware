2508a2f5f518b65152339dfbe057f4caccbedea6
Bram Soenens <bramsoenens@hotmail.be>
2020-02-17 11:15:05 +0100

    address update I2C !BIT

--- TPH/maini2c.cpp
+++ TPH/maini2c.cpp
@@ -3,7 +3,9 @@
 //#include "i2cTPH.h"
 
 //For IC2 intilization
-BME280 TphIc2 = BME280(D1, D0, 1110110); 
+
+char addr = 0x76 << 1; //OPGEPAST Mbed gebruikt 8 bit I2C addressen
+BME280 TphIc2 = BME280(D1, D0, addr); 
 //DO en D1 voor NUCLEO_L432KC
 //D14 en D15 voor NUCLEO_F042K6
 
