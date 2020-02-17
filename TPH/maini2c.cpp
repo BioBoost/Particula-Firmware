9a78bbac5c5a3158acf264c98ba7c854516762ec
Bram Soenens <bramsoenens@hotmail.be>
2020-02-17 10:41:58 +0100

    address update I2C

--- TPH/maini2c.cpp
+++ TPH/maini2c.cpp
@@ -3,7 +3,7 @@
 //#include "i2cTPH.h"
 
 //For IC2 intilization
-BME280 TphIc2 = BME280(D1, D0, DEFAULT_SLAVE_ADDRESS); 
+BME280 TphIc2 = BME280(D1, D0, 1110110); 
 //DO en D1 voor NUCLEO_L432KC
 //D14 en D15 voor NUCLEO_F042K6
 
@@ -13,7 +13,6 @@ Serial pc(USBTX, USBRX); //tx, rx
 
 int main(){
     pc.printf("\n IC2");
-    printf("TEST");
     pc.printf("\n Humidity = %lf", TphIc2.getHumidity());
     pc.printf("\n Temperature = %lf", TphIc2.getTemperature());
     pc.printf("\n Presure = %lf", TphIc2.getPressure());
