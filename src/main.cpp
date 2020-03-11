e43f8be55f484f9428c8a8d55ff0ec451492461b
Maxim Vanden Abeele <maxim.vandenabeele@gmail.com>
2020-03-11 12:20:08 +0100

    move object initialization outside of main

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,16 @@
 #include "BME280.h"
 
 SimpleLoRaWAN::Node node(keys, pins);   // If placed in main, stack size probably too small
+BME280 tph_sensor = BME280(D14, D15, 0x76 << 1); // D4 en D5 voor kleine nucleo
+ParticulaLora::AmbiantSensorMessage message;
 
 int main(void) {
-
-    char addr = 0x76 << 1;  // I2C address of TPH sensor
-    BME280 tph_sensor = BME280(D14, D15, addr); // D4 en D5 voor kleine nucleo
-
     while (true) {
         double temperature = (double) tph_sensor.getTemperature();  // value in °C
         double humidity = (double) tph_sensor.getHumidity();        // value in %
         double pressure = (double) tph_sensor.getPressure();        // value in hPa
         double pm25 = 12.3;          // value in µg/m³
         double pm10 = 23.4;          // value in µg/m³
-
-        ParticulaLora::AmbiantSensorMessage message;
 
         temperature = (double) tph_sensor.getTemperature();
         humidity = (double) tph_sensor.getHumidity();
@@ -31,7 +27,7 @@ int main(void) {
         message.addPM(pm10);
 
         node.send(message.getMessage(), message.getLength());
-        wait(30.0);
+        ThisThread::sleep_for(30000);
     }
     return 0;
 }
