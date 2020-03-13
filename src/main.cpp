c1eca33cc62552f25b6a04d508f19a90fb4d7136
Maxim Vanden Abeele <maxim.vandenabeele@gmail.com>
2020-03-13 12:09:18 +0100

    try to add fprint to firmware

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,32 @@
 #include "Simple-LoRaWAN.h"
 #include "settings.h"
 #include "BME280.h"
+#include <stdio.h>
 
 SimpleLoRaWAN::Node node(keys, pins);   // If placed in main, stack size probably too small
 BME280 tph_sensor = BME280(D14, D15, 0x76 << 1); // D4 en D5 voor kleine nucleo
-ParticulaLora::AmbiantSensorMessage message;
+
+// Serial pc(USBTX, USBRX);
 
 int main(void) {
+    // pc.baud(115200);
+    // pc.printf("\n[Particula] Loading Firmware ...\n");
+
     while (true) {
+        ParticulaLora::AmbiantSensorMessage message;
+
+        // pc.printf("\n[Particula] Reading temperature: ...");
         double temperature = (double) tph_sensor.getTemperature();  // value in °C
+        // pc.printf("\n[Particula] Measered temperature: %d", temperature);
+
         double humidity = (double) tph_sensor.getHumidity();        // value in %
         double pressure = (double) tph_sensor.getPressure();        // value in hPa
         double pm25 = 12.3;          // value in µg/m³
         double pm10 = 23.4;          // value in µg/m³
 
-        temperature = (double) tph_sensor.getTemperature();
-        humidity = (double) tph_sensor.getHumidity();
-        pressure = (double) tph_sensor.getPressure();
+        // temperature = (double) tph_sensor.getTemperature();
+        // humidity = (double) tph_sensor.getHumidity();
+        // pressure = (double) tph_sensor.getPressure();
 
         message.addTemperature(temperature);
         message.addHumidity(humidity);
