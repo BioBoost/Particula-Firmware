e51b65f9542ed8f42199d50997fb0e38fcd680eb
Martijn Everaert <martijn.everaert@gmail.com>
2020-04-27 15:48:02 +0200

    part 1 of merging develop and final-form

--- src/main.cpp
+++ src/main.cpp
@@ -4,57 +4,58 @@
 #include "settings.h"
 #include "BME280.h"
 #include "SDS011.h"
+#include <exception>
 
-Serial pc(USBTX, USBRX);
-
-using namespace Particula;
+SimpleLoRaWAN::Node node(keys, pins);   // If placed in main, stack size probably too small (Results in Fatal Error)
+BME280 tph_sensor = BME280(D14, D15, 0x76 << 1); // D4 en D5 voor kleine nucleo
+SDS011_Particle::SDS011 part_sensor(D1, D0);  // D1 en D0 voor kleine nucleo
 
 int main(void) {
-    SimpleLoRaWAN::Node node(keys, pins);   // If placed in main, stack size probably too small (Results in Fatal Error)
-    
-    BME280 tph_sensor = BME280(
-        I2C_SDA_PIN,
-        I2C_SCK_PIN,
-        0x76 << 1
-    );
 
-    SDS011 part_sensor(UART_TX_PIN, UART_RX_PIN);
-    
-    pc.printf("\r\n\r\n[Particula] Loading Firmware ...");
+    const int PART_SENSOR_WAKEUP_ERROR = 0x00;
+    const int PART_SENSOR_READ_ERROR = 0x01;
+    const int PART_SENSOR_SLEEP_ERROR = 0x02;
+
 
     while (true) {
-        AmbiantSensorMessage message;    // Must be placed here, new values will otherwise be added to the same message
-        pc.printf("\r\n[Particula] Taking measurements ...\r\n");
+        ParticulaLora::AmbiantSensorMessage message;
 
-        int sds011WakeUpStatus = part_sensor.wakeUp();
-        int sds011ReadStatus = part_sensor.read();
+        double percentage = readBattery();
 
-        double temperature = (double) tph_sensor.getTemperature();  // value in °C
-        double humidity = (double) tph_sensor.getHumidity();        // value in %
-        double pressure = (double) tph_sensor.getPressure();        // value in hPa
-        double pm25 = part_sensor.getPM25Value();          // value in µg/m³
-        double pm10 = part_sensor.getPM10Value();          // value in µg/m³
+        if(percentage >= 20.0){   
+            if( part_sensor.wakeUp() == WAKEUP_NOT_SUCCESFULL ){}        
+                message.addError(PART_SENSOR_WAKEUP_ERROR);
+            }
 
-        pc.printf("[Particula] Measered temperature:  %4.2f °C\r\n", temperature);
-        pc.printf("[Particula] Measered humidity:     %4.2f %%\r\n", humidity);
-        pc.printf("[Particula] Measered pressure:     %4.2f hPa\r\n", pressure);
-        pc.printf("[Particula] Measered PM25:         %4.2f µg/m3\r\n", pm25);
-        pc.printf("[Particula] Measered PM10:         %4.2f µg/m3\r\n", pm10);
+            ThisThread::sleep_for(30000);     
+            tph_sensor.awake();
+            
+            if(part_sensor.read() == READ_SUCCESSFULL){
+                message.addPM(part_sensor.getPM25Value());
+                message.addPM(part_sensor.getPM10Value());
+            } else {
+                message.addError(PART_SENSOR_READ_ERROR);
+            }
 
-        message.addTemperature(temperature);
-        message.addHumidity(humidity);
-        message.addPressure(pressure);
-        message.addPM(pm25);
-        message.addPM(pm10);
+            message.addTemperature((double) tph_sensor.getTemperature());
+            message.addHumidity((double) tph_sensor.getHumidity());
+            message.addPressure((double) tph_sensor.getPressure());
 
-        // Add status (error) codes
-        message.addError(sds011WakeUpStatus);
-        message.addError(sds011ReadStatus);
-
-        node.send(message.getMessage(), message.getLength());
-        part_sensor.sleep();
-        pc.printf("[Particula] Going to sleep, deep sleep possible (1: yes, 0: no): %i\r\n", sleep_manager_can_deep_sleep());
-        ThisThread::sleep_for(30000);
+            if(part_sensor.sleep() == SLEEP_NOT_SUCCESFULL){
+            } 
+                message.addError(PART_SENSOR_SLEEP_ERROR);
+            }
+            tph_sensor.sleep();
+            node.send(message.getMessage(), message.getLength());           
+                  
+        } else if(percentage <= 20.0){
+            ThisThread::sleep_for(30000);
+        }
+        ThisThread::sleep_for(270000); 
     }
     return 0;
+}
+
+double readBattery(){
+    return 80.0;
 }
